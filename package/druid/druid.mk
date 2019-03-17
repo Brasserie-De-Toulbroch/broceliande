6b8d4e11d0cce9cba819f12555d8db0068a9dc98
Blottiere Paul <blottiere.paul@gmail.com>
2019-03-17 12:49:40 +0100

    Fixes druid compilation dependencies

--- package/druid/druid.mk
+++ package/druid/druid.mk
@@ -9,5 +9,6 @@ DRUID_SITE = https://github.com/Brasserie-De-Toulbroch/druid
 DRUID_SITE_METHOD = git
 DRUID_INSTALL_STAGING = NO
 DRUID_INSTALL_TARGET = YES
+DRUID_DEPENDENCIES = qt5base qwt
 
 $(eval $(cmake-package))
